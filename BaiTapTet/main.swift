1fde77f03cfbf2a4e3c6a916ed69a1d868085138
Toan <toan@Toans-MBP.net.fpt>
2020-02-02 10:25:12 -0800

    first commit

--- BaiTapTet/main.swift
+++ BaiTapTet/main.swift
@@ -7,4 +7,14 @@
 //
 
 import Foundation
-timSoLeLonNhat()
+Bai1() // bai 1
+timNgayThangNam() // bai 3
+Amstrong() // bai 4
+demSoNguyenTo(1, 5) // bai5
+chenGiaChi() // bai 6
+Fibonanci() // bai 7
+Upercase()// bai 9
+chuoiDoiXung() //bai10
+mangSoNguyen() // bai 11
+timso() // bai 12
+oChinhPhuong() // bai 13
